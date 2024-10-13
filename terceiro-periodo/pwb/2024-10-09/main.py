7224edd9a9d01cd075a900bdd18ba4beecf8740f
davidplay7 <davidk1k3k@gmail.com>
2024-10-13 11:22:15 -0300

    Finalizando api pwb

--- terceiro-periodo/pwb/2024-10-09/main.py
+++ terceiro-periodo/pwb/2024-10-09/main.py
@@ -1,3 +1,5 @@
+# fastapi dev main.py
+
 from fastapi import FastAPI, status, HTTPException
 
 from pydantic import BaseModel
@@ -10,11 +12,13 @@ class Clube(BaseModel):
 
 clubes_futebol: list[Clube] = []
 
+# All clubes
 @app.get("/clubes", status_code=status.HTTP_200_OK)
 def listar_clubes():
     return clubes_futebol
 
-@app.get("/clubes/{clube_id}")
+# One Clube
+@app.get("/clubes/{clube_id}", status_code=status.HTTP_200_OK)
 def detalhes_clube(clube_id: int):
     for clube in clubes_futebol:
         if clube_id == clube.id:
@@ -25,18 +29,26 @@ def detalhes_clube(clube_id: int):
         detail=f"Clube não localizado com id = {clube_id}"
     )
 
+# Post clube
 @app.post("/clubes", status_code=status.HTTP_201_CREATED)
 def adicionar_clube(novo_clube: Clube):
     clubes_futebol.append(novo_clube)
     return clubes_futebol
 
+# Put clube
 @app.put("/clubes/{clube_id}", status_code=status.HTTP_200_OK)
-def alterar_clube(clube_id: int, clube: Clube):
+async def alterar_clube(clube_id: int, clube: Clube):
     clubes_futebol[clube_id] = clube
     return clubes_futebol
-    
-@app.delete("/clubes/{clube_id}", status_code=status.HTTP_200_OK)
-def alterar_clube(clube_id: int):
-    clubes_futebol.pop(clube_id)
+
+# Delete all clubes
+@app.delete("/clubes/", status_code=status.HTTP_200_OK)
+def deletar_clubes():
+    clubes_futebol.clear()
     return clubes_futebol    
 
+# Delete one clube
+@app.delete("/clubes/{clube_id}", status_code=status.HTTP_200_OK)
+def deletar_clube(clube_id: int):
+    clubes_futebol.pop(clube_id)
+    return clubes_futebol 
---
+++ terceiro-periodo/pwb/index.md
@@ -0,0 +1,16 @@
+# Primeiros passos FastApi
+
+* Instalar e ativar o ambiente virtual
+```sh
+    python3 -m venv venv
+    source venv/bin/activate  # No Windows, use `venv\Scripts\activate`
+```
+
+* Instalar os pacotes
+```sh
+    pip install -r requirements.txt
+```
+* Desativar o ambiente virtual
+```sh
+    deactivate
+```
